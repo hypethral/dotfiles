c89e056e8727afaa1e2614263c9b4958c3c16812
hypethral <hypethral1@gmail.com>
2022-04-22 18:31:20 +0500

    Removed folders

--- Code - OSS/User/globalStorage/iocave.monkey-patch/modules/main.js
+++
@@ -1,7 +0,0 @@
-require.config({
-    paths: {
-        "monkey-generated" : "/home/dark/.config/Code - OSS/User/globalStorage/iocave.monkey-patch/modules"
-    }
-});
-
-define(["monkey-generated/entrypoint-main"], function (){});
